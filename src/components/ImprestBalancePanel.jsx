fb0b7485eeae98d51ba161a255fe353d8d523308
HasalKenula <hasalkenula255@gmail.com>
2026-09-07 20:27:44 +0530

    changed total to balance name in imprest balance pdf

--- src/components/ImprestBalancePanel.jsx
+++ src/components/ImprestBalancePanel.jsx
@@ -386,7 +386,7 @@ const ImprestBalancePanel = () => {
       doc.setFontSize(12);
       doc.setFont('helvetica', 'bold');
       startY += 7;
-      doc.text(`Grand Total:`, 25, startY);
+      doc.text(`Balance:`, 25, startY);
       doc.text(`${formatNumber(totalGrand)}`, 190, startY, { align: 'right' });
       startY += 10;
 
@@ -414,7 +414,7 @@ const ImprestBalancePanel = () => {
 
       // Table headers
       const tableHeaders = [
-        'TR No', 'Opening Balance', 'DR Amount', 'Issue Amount', 'CR Amount', 'Settle Amount', 'Grand Total'
+        'Head', 'Opening Balance', 'DR Amount', 'Issue Amount', 'CR Amount', 'Settle Amount', 'Grand Total'
       ];
 
       const tableBody = records.map(record => [
